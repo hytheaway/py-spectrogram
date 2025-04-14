3848d9b936190c480d3da8b50667f7fd990e28c7
hytheaway <91982175+hytheaway@users.noreply.github.com>
2025-04-14 17:46:01 -0400

    proper matplotlib implementation

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from scipy import signal
 wavfile_path = input('Path to wav file: ')
 start_ms = input('Start time (ms): ')
 end_ms = input('End time (ms): ')
+plot_title = input('Plot title: ')
+
+if not plot_title:
+    plot_title = str(wavfile_path.split("/")[len(wavfile_path.split("/"))-1]) # python should convert this to whatever os
 
 sr, samples = wavfile.read(str(wavfile_path))
 start_in_samples = (float(start_ms)/1000) * sr
@@ -21,8 +25,13 @@ rebound_samples = samples[start_in_samples:end_in_samples]
 
 f, t, spectrogram = signal.spectrogram(rebound_samples, sr)
 
-plt.pcolormesh(t, f, 10*np.log10(spectrogram), shading='gouraud')
-plt.ylabel('Frequency (Hz)')
-plt.xlabel('Time (s)')
-plt.colorbar(label='Intensity (dB)')
+fig, ax = plt.subplots()
+p = ax.pcolormesh(t, f, 10*np.log10(spectrogram), shading='gouraud')
+ax.set_ylim(1, int(sr/2))
+ax.set_ylabel('Frequency (Hz)')
+ax.set_xlabel('Time (s)')
+
+fig.colorbar(p, label='Intensity (dB)')
+
+plt.title(str(plot_title))
 plt.show()
